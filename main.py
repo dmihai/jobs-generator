3a2081ef7277e74134e84ca8f8f53028471932fc
Mihai Doarna <dmihai@gmail.com>
2022-06-05 21:41:19 +0300

    add more dimensions

--- main.py
+++ main.py
@@ -13,13 +13,18 @@ version = 3
 jobs = []
 for asset in constants.common_assets:
     for year in constants.years:
-        for timeframe in ['M5', 'M10']:
-            params = {}
-            jobs.append({
-                "asset": asset,
-                "year": year,
-                "timeframe": timeframe,
-                "params": json.dumps(params)
-            })
+        for timeframe in ["M5"]:
+            for start_offset in [3, 2, 1]:
+                for stop_offset in [3, 2, 1]:
+                    params = {
+                        "start_offset": start_offset,
+                        "stop_offset": stop_offset
+                    }
+                    jobs.append({
+                        "asset": asset,
+                        "year": year,
+                        "timeframe": timeframe,
+                        "params": json.dumps(params)
+                    })
 
 db.insert_jobs('TestStrategy', version, jobs)
